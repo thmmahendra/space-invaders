28434eb80d093c3ffe6233de974381c17ecd5149
thmmahendra <thmmahendra@gmail.com>
2024-01-17 09:02:41 +0545

    Initial Commit

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ function animate() {
         }
     })
 
-    grids.forEach(grid => {
+    grids.forEach((grid, gridIndex) => {
         grid.update()
         grid.invaders.forEach((invader, i) => {
             invader.update({ velocity: grid.velocity })
@@ -222,9 +222,20 @@ function animate() {
 
                         const projectileFound = projectiles.find(projectile2 => projectile2 === projectile)
 
+                        // remove invader and projectile
                         if (invaderFound && projectileFound) {
                             grid.invaders.splice(i, 1)
                             projectiles.splice(j, 1)
+
+                            if (grid.invaders.length > 0) {
+                                const firstInvader = grid.invaders[0]
+                                const lastInvader = grid.invaders[grid.invaders.length - 1]
+
+                                grid.width = lastInvader.position.x - firstInvader.position.x + lastInvader.width
+                                grid.position.x = firstInvader.position.x
+                            } else {
+                                grid.splice(gridIndex, 1)
+                            }
                         }
                     }, 0)
                 }
