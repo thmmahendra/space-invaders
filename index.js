6f48a31af0a4d7644754ea869a8d7fb38d998d76
thmmahendra <thmmahendra@gmail.com>
2024-01-17 08:05:27 +0545

    Initial Commit

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ class Grid {
 
 const player = new Player()
 const projectiles = []
-const grids = [new Grid()]
+const grids = []
 const keys = {
     a: {
         pressed: false
@@ -185,6 +185,10 @@ const keys = {
         pressed: false
     }
 }
+
+let frames = 0
+let randomInterval = Math.floor((Math.random() * 500) + 500)
+console.log(randomInterval)
 
 function animate() {
     requestAnimationFrame(animate)
@@ -223,6 +227,15 @@ function animate() {
         player.velocity.x = 0
         player.rotation = 0
     }
+    console.log(frames)
+    // spawning enemies
+    if (frames % randomInterval === 0) {
+        grids.push(new Grid())
+        randomInterval = Math.floor((Math.random() * 500) + 500)
+        frames = 0
+        console.log(randomInterval)
+    }
+    frames++
 }
 
 animate()
